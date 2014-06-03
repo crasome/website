ec31ee83afa00afb1b4c4a4e62e62d0c5ef3d9db
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-06-03 12:37:56 +0300

    Message subject is optional for requests

--- app/forms/contact/hire_request.rb
+++ app/forms/contact/hire_request.rb
@@ -9,7 +9,8 @@ module Contact
     attribute :visitor, Visitor, default: Visitor.new
     attribute :company, Company, default: Company.new
 
-    validates "message.title",  presence: true, length: { minimum: 5, maximum: 300 }
+    validates "message.title",                     length: { minimum: 5, maximum: 256 }, allow_blank: true
+    validates "message.content",  presence: true,  length: { mimumin: 10,  maximum: 5000 }
 
     validates "company.email", email: true, allow_blank: true
     validates "visitor.email", email: true, allow_blank: true
--- app/forms/contact/join_request.rb
+++ app/forms/contact/join_request.rb
@@ -8,7 +8,7 @@ module Contact
     attribute :message,  Message,   default: Message.new
     attribute :visitor,  Engineer,  default: Engineer.new
 
-    validates "message.title",    presence: true,  length: { minimum: 5,   maximum: 200 }
+    validates "message.title",                     length: { minimum: 5,   maximum: 256 }, allow_blank: true
     validates "message.content",  presence: true,  length: { mimumin: 10,  maximum: 5000 }
 
     validates "visitor.position",  presence: true,  length: { minimum: 5,  maximum: 100 }
